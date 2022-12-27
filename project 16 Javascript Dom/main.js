41424d80c414206031a396e8fa7ffe8ced276269
Luka Semenikhin <117368298+lukasemenikhin@users.noreply.github.com>
2022-12-27 16:06:37 +0400

    Add files via upload

---
+++ project 16 Javascript Dom/main.js
@@ -0,0 +1,6 @@
+function Changetext() {
+    let title = document.getElementById('title');
+    title.innerHTML = 'My page';
+    title = document.getElementById('text');
+    title.innerHTML = 'How i create my page';
+}
